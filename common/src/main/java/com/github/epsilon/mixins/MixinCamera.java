d1627626e7042231f4b3b3c8a37bcc2b8f449a15
ilove0329P <hhcrystal2008@gmail.com>
2026-06-07 01:19:49 +0800

    补齐 CameraClip Action 模式 (#210)

--- common/src/main/java/com/github/epsilon/mixins/MixinCamera.java
+++ common/src/main/java/com/github/epsilon/mixins/MixinCamera.java
@@ -2,10 +2,16 @@ package com.github.epsilon.mixins;
 
 import com.github.epsilon.modules.impl.render.CameraClip;
 import net.minecraft.client.Camera;
+import net.minecraft.client.CameraType;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+import static com.github.epsilon.Constants.mc;
 
 @Mixin(Camera.class)
 public class MixinCamera {
@@ -15,6 +21,25 @@ public class MixinCamera {
         CameraClip cameraClip = CameraClip.INSTANCE;
         if (cameraClip.isEnabled()) {
             cir.setReturnValue(cameraClip.distance.getValue().floatValue());
+        }
+    }
+
+    @ModifyArgs(method = "alignWithEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Camera;setPosition(DDD)V"))
+    private void hookActionCameraPosition(Args args) {
+        CameraClip cameraClip = CameraClip.INSTANCE;
+        if (cameraClip.isEnabled() && cameraClip.action.getValue()) {
+            if (mc.options.getCameraType() == CameraType.THIRD_PERSON_BACK) {
+                Vec3 targetPos = new Vec3(args.get(0), args.get(1), args.get(2));
+                cameraClip.updateActionCamera(targetPos);
+                Vec3 cameraPos = cameraClip.getCameraPos();
+                if (cameraPos != null) {
+                    args.set(0, cameraPos.x);
+                    args.set(1, cameraPos.y);
+                    args.set(2, cameraPos.z);
+                }
+            } else {
+                cameraClip.resetCameraPos();
+            }
         }
     }
 
--- common/src/main/java/com/github/epsilon/modules/impl/render/CameraClip.java
+++ common/src/main/java/com/github/epsilon/modules/impl/render/CameraClip.java
@@ -4,6 +4,8 @@ import com.github.epsilon.modules.Category;
 import com.github.epsilon.modules.Module;
 import com.github.epsilon.settings.impl.BoolSetting;
 import com.github.epsilon.settings.impl.DoubleSetting;
+import net.minecraft.util.Mth;
+import net.minecraft.world.phys.Vec3;
 
 public class CameraClip extends Module {
 
@@ -14,8 +16,50 @@ public class CameraClip extends Module {
     }
 
     public final DoubleSetting distance = doubleSetting("Distance", 3.5, 1.0, 20.0, 0.5);
-
     public final BoolSetting action = boolSetting("Action", true);
-    private final DoubleSetting interpolation = doubleSetting("Interpolation", 0.05, 0.01, 1.0, 0.01, action::getValue);
+    public final DoubleSetting actionSmoothness = doubleSetting("Action Smoothness", 0.3, 0.1, 0.95, 0.01, action::getValue);
+    public final DoubleSetting actionMaxDistance = doubleSetting("Action Max Distance", 20.0, 1.0, 50.0, 0.5, action::getValue);
+
+    private Vec3 cameraPos;
+
+    @Override
+    protected void onEnable() {
+        cameraPos = null;
+    }
+
+    @Override
+    protected void onDisable() {
+        cameraPos = null;
+    }
+
+    public void updateActionCamera(Vec3 targetPos) {
+        if (cameraPos == null) {
+            cameraPos = targetPos;
+            return;
+        }
+
+        double distanceToTarget = cameraPos.distanceTo(targetPos);
+        double maxDistance = actionMaxDistance.getValue();
+        if (distanceToTarget > maxDistance) {
+            cameraPos = targetPos;
+            return;
+        }
+
+        double smoothFactor = actionSmoothness.getValue() * (1.0 - Math.exp(-distanceToTarget / maxDistance));
+
+        cameraPos = new Vec3(
+                Mth.lerp(smoothFactor, cameraPos.x, targetPos.x),
+                Mth.lerp(smoothFactor, cameraPos.y, targetPos.y),
+                Mth.lerp(smoothFactor, cameraPos.z, targetPos.z)
+        );
+    }
+
+    public void resetCameraPos() {
+        cameraPos = null;
+    }
+
+    public Vec3 getCameraPos() {
+        return cameraPos;
+    }
 
 }
