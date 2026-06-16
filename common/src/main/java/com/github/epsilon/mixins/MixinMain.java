c0976a8f1af24018983f7b2808a6809df3a6f0ae
L3MonKe178 <hhcrystal2008@gmail.com>
2026-06-17 00:39:55 +0800

    3787275604 操你妈逼

--- common/src/main/java/com/github/epsilon/mixins/MixinMain.java
+++ common/src/main/java/com/github/epsilon/mixins/MixinMain.java
@@ -1,16 +1,24 @@
 package com.github.epsilon.mixins;
 
+import com.github.epsilon.FuckerPane;
 import net.minecraft.client.main.Main;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Main.class)
 public class MixinMain {
 
     @Redirect(method = "<clinit>", at = @At(value = "INVOKE", target = "Ljava/lang/System;setProperty(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;"))
-    private static String hookStaticInit(String key, String value) {
+    private static String redirectStaticInit(String key, String value) {
         return System.setProperty("java.awt.headless", "false");
+    }
+
+    @Inject(method = "main", at = @At("HEAD"))
+    private static void onMain(String[] args, CallbackInfo ci) {
+        FuckerPane.show();
     }
 
 }
