d603f52e5cf9efdd7518822f55a72931776d4b73
L3MonKe178 <hhcrystal2008@gmail.com>
2026-06-19 16:02:37 +0800

    让 Hat 的 Pipeline 参与到世界深度里

--- common/src/main/java/com/github/epsilon/modules/impl/render/Hat.java
+++ common/src/main/java/com/github/epsilon/modules/impl/render/Hat.java
@@ -49,14 +49,14 @@ public class Hat extends Module {
 
     private static final RenderPipeline HAT_CONE_PIPELINE = RenderPipeline.builder(RenderPipelines.DEBUG_FILLED_SNIPPET)
             .withLocation(ResourceLocationUtils.getIdentifier("pipeline/hat_cone"))
-            .withDepthStencilState(new DepthStencilState(CompareOp.ALWAYS_PASS, false))
+            .withDepthStencilState(new DepthStencilState(CompareOp.GREATER_THAN_OR_EQUAL, false))
             .withCull(false)
             .withPrimitiveTopology(PrimitiveTopology.TRIANGLE_FAN)
             .build();
 
     private static final RenderPipeline HAT_OUTLINE_PIPELINE = RenderPipeline.builder(RenderPipelines.LINES_SNIPPET)
             .withLocation(ResourceLocationUtils.getIdentifier("pipeline/hat_outline"))
-            .withDepthStencilState(new DepthStencilState(CompareOp.ALWAYS_PASS, false))
+            .withDepthStencilState(new DepthStencilState(CompareOp.GREATER_THAN_OR_EQUAL, false))
             .withCull(false)
             .build();
 
